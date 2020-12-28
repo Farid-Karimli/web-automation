021e44095025a82930d2f067dbd91769c97eea70
Farid Karimli <karimli.farid@icloud.com>
2020-12-28 09:39:10 -0500

    Add files via upload
    Still working on this script, as it doesn't work fully. Trying to select and click on multiple indistinguishable elements in order.

---
+++ patientconnect.js
@@ -0,0 +1,77 @@
+const puppeteer = require("puppeteer");
+const { Keyboard } = require("puppeteer/lib/cjs/puppeteer/common/Input");
+
+var url = "http://patientconnect.bu.edu/";
+
+async function patientconnect(url) {
+
+    const browser = await puppeteer.launch(  { headless: false}  );
+    const page = await browser.newPage();
+
+    page.goto(url);
+    await page.waitForNavigation();
+
+    await page.waitForTimeout(1000);
+    await page.type('#j_username', 'faridkar', {delay: 10});
+    await page.type('#j_password', 'Ganer-Baner1', {delay: 10});
+    await page.click("button.input-submit");
+    await page.waitForNavigation();
+
+    await page.click("a.btn.btn-sm.btn-primary");
+    // await page.waitForNavigation();
+
+    // await page.waitForTimeout(500);
+    await page.waitForSelector("a.btn.btn-lg.btn-success");
+    await page.click("a.btn.btn-lg.btn-success");
+    // await page.waitForNavigation();
+
+    await page.waitForSelector("div.answer.button.p-3");
+
+
+    await page.evaluate ( () => {
+
+        // Array.from(document.querySelectorAll("div.answer.button.p-3"))
+
+        let no_buttons = document.querySelectorAll("div.answer.button.p-3");
+        for (let button of no_buttons) {
+
+            button.click();
+    
+        }
+
+    });
+
+
+
+    
+
+    
+
+
+
+}
+
+
+
+
+
+
+patientconnect(url);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
